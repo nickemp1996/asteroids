43431d4edc66ca80c89f23177f3f5f3a9a820035
nickemp1996 <nickemp1996@gmail.com>
2025-02-21 20:36:39 -0700

    added a player class and groups to update and draw

--- main.py
+++ main.py
@@ -3,19 +3,35 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0
+	x = SCREEN_WIDTH / 2
+	y = SCREEN_HEIGHT / 2
+	updatables = pygame.sprite.Group()
+	drawables = pygame.sprite.Group()
+	Player.containers = (updatables, drawables)
+	player1 = Player(x, y)
 	while 1:
 		for event in pygame.event.get():
-    		if event.type == pygame.QUIT:
-        		return
-		screen.fill((0, 0, 0))
+			if event.type == pygame.QUIT:
+				return
+		
+		updatables.update(dt)
+		#player1.update(dt)
+
+		screen.fill("black")
+		for drawable in drawables:
+			drawable.draw(screen)
+		#player1.draw(screen)
 		pygame.display.flip()
-	# print("Starting Asteroids!")
-	# print(f"Screen width: {SCREEN_WIDTH}")
-	# print(f"Screen height: {SCREEN_HEIGHT}")
+		
+		# limit the framerate to 60 FPS
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
