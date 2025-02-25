26738b96ab726b5fe856a91bf48e6f22560b5899
nickemp1996 <nickemp1996@gmail.com>
2025-02-24 19:33:32 -0700

    added asteroids that we can shoot. Shooting large asteroids makes two medium asteroids; shooting medium asteroids makes two small asteroids; shooting small asteroids destroys them.

--- main.py
+++ main.py
@@ -1,33 +1,54 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
-from player import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
+
 	dt = 0
+
 	x = SCREEN_WIDTH / 2
 	y = SCREEN_HEIGHT / 2
+
 	updatables = pygame.sprite.Group()
 	drawables = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
+
 	Player.containers = (updatables, drawables)
+	Asteroid.containers = (asteroids, updatables, drawables)
+	AsteroidField.containers = updatables
+	Shot.containers = (shots, updatables, drawables)
+
 	player1 = Player(x, y)
+	asteroid_field = AsteroidField()
+
 	while 1:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		
 		updatables.update(dt)
-		#player1.update(dt)
+
+		for asteroid in asteroids:
+			if asteroid.collision_detection(player1):
+				print("Game Over!")	
+				exit()	
+
+			for shot in shots:
+				if asteroid.collision_detection(shot):
+					asteroid.split()
+					shot.kill()
 
 		screen.fill("black")
-		for drawable in drawables:
-			drawable.draw(screen)
-		#player1.draw(screen)
+		for obj in drawables:
+			obj.draw(screen)
+		
 		pygame.display.flip()
 		
 		# limit the framerate to 60 FPS
--- player.py
+++ player.py
@@ -1,10 +1,13 @@
-from circleshape import *
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+import pygame
+from circleshape import CircleShape
+from constants import *
+from shot import Shot
 
 class Player(CircleShape):
 	def __init__(self, x, y):
 		super().__init__(x, y, PLAYER_RADIUS)
 		self.rotation = 0
+		self.shoot_timer = 0
 
 	# in the player class
 	def triangle(self):
@@ -27,6 +30,7 @@ class Player(CircleShape):
 
 	def update(self, dt):
 		keys = pygame.key.get_pressed()
+		self.shoot_timer -= dt
 
 		if keys[pygame.K_a]:
 			self.rotate(-dt)
@@ -36,3 +40,13 @@ class Player(CircleShape):
 			self.move(-dt)
 		if keys[pygame.K_w]:
 			self.move(dt)
+		if keys[pygame.K_SPACE]:
+			self.shoot()
+				
+
+	def shoot(self):
+		if self.shoot_timer > 0:
+			return
+		self.shoot_timer = PLAYER_SHOOT_COOLDOWN
+		my_shot = Shot(self.position.x, self.position.y)
+		my_shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOT_SPEED
